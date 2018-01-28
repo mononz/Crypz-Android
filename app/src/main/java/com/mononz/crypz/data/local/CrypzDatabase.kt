8f69e9b1bd3a502cb74f85938320ce38cf179eda
Jared Hall <jaredrexhall@gmail.com>
2018-01-28 20:32:38 +1100

    Adding setting entity and dao to app and including in sync

--- app/src/main/java/com/mononz/crypz/data/local/CrypzDatabase.kt
+++ app/src/main/java/com/mononz/crypz/data/local/CrypzDatabase.kt
@@ -4,20 +4,15 @@ import android.app.Application
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
+import com.mononz.crypz.data.local.dao.*
 
-import com.mononz.crypz.data.local.dao.CoinDao
-import com.mononz.crypz.data.local.dao.MarketCoinDao
-import com.mononz.crypz.data.local.dao.MarketDao
-import com.mononz.crypz.data.local.dao.StakeDao
-import com.mononz.crypz.data.local.entity.CoinEntity
-import com.mononz.crypz.data.local.entity.MarketCoinEntity
-import com.mononz.crypz.data.local.entity.MarketEntity
-import com.mononz.crypz.data.local.entity.StakeEntity
+import com.mononz.crypz.data.local.entity.*
 
 @Database(entities = [
     (CoinEntity::class),
     (MarketEntity::class),
     (MarketCoinEntity::class),
+    (SettingEntity::class),
     (StakeEntity::class)],
         version = 1)
 abstract class CrypzDatabase : RoomDatabase() {
@@ -25,6 +20,7 @@ abstract class CrypzDatabase : RoomDatabase() {
     abstract fun coinDao(): CoinDao
     abstract fun marketDao(): MarketDao
     abstract fun marketCoinDao(): MarketCoinDao
+    abstract fun settingDao(): SettingDao
     abstract fun stakeDao(): StakeDao
 
     companion object {
